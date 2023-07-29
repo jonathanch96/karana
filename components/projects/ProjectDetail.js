f633930a28517fb8f4de7fedc18d7eab314436e4
Jonathan <jonathanchang96@gmail.com>
2023-07-29 19:22:12 +0700

    project detail

--- components/projects/ProjectDetail.js
+++ components/projects/ProjectDetail.js
@@ -1,46 +1,68 @@
 import Image from 'next/image'
 import styles from './ProjectDetail.module.css'
 import ProjectDetailImage from './ProjectDetailImage'
+import projects from '../../data/projects.json'
+import { useRouter } from 'next/router'
+import Error from 'next/error'
 const ProjectDetail = () => {
+  const router = useRouter()
+  const { id } = router.query
+
+  // Find the project data in the projects array
+  const project = projects.find((project) => project.link === `/projects/${id}`)
+
+  if (!project) {
+    // Project not found, return a 404 page
+    return <Error statusCode={404} />
+  }
+
   return (
-    <div className={styles['container']}>
-      <div className={styles['project-description-container']}>
-        <div className={styles['project-title-container']}>
-          <div className={styles['label']}>Project</div>
-          <div className={`${styles['title']} ${styles['project-title']}`}>
-            Bening's Clinic
-          </div>
-        </div>
-        <div className={styles['description-container']}>
-          <div className={styles['title-container']}>
-            <div className={styles['label']}>Location</div>
-            <div className={styles['description']}>Cibubur, Indonesia</div>
-          </div>
-          <div className={styles['title-container']}>
-            <div className={styles['label']}>Years</div>
-            <div className={styles['description']}>2022</div>
-          </div>
-          <div className={styles['title-container']}>
-            <div className={styles['label']}>Client</div>
-            <div className={styles['description']}>Bening Clinic</div>
-          </div>
-          <div className={styles['title-container']}>
-            <div className={styles['label']}>Team</div>
-            <div className={styles['description']}>
-              Datu Aria Kandhabuana, Iman Diman Gissa Nurhalisa
+    <div className="container-background">
+      <Image
+        className={styles['image-background']}
+        width={1440}
+        height={4064}
+        src="/images/background_detail.png"
+      />
+      <div className={styles['container']}>
+        <div className={styles['project-description-container']}>
+          <div className={styles['project-title-container']}>
+            <div className={styles['label']}>Project</div>
+            <div className={`${styles['title']} ${styles['project-title']}`}>
+              {project.project}
             </div>
           </div>
-          <div className={styles['title-container']}>
-            <div className={styles['label']}>Category</div>
-            <div className={styles['description']}>
-              Architecture, Interior Design, Commecrial, Beauty Clinc & Skincare
+          <div className={styles['description-container']}>
+            <div className={styles['title-container']}>
+              <div className={styles['label']}>Location</div>
+              <div className={styles['description']}>{project.location}</div>
+            </div>
+            <div className={styles['title-container']}>
+              <div className={styles['label']}>Years</div>
+              <div className={styles['description']}>{project.year}</div>
+            </div>
+            <div className={styles['title-container']}>
+              <div className={styles['label']}>Client</div>
+              <div className={styles['description']}>{project.client}</div>
+            </div>
+            <div className={styles['title-container']}>
+              <div className={styles['label']}>Team</div>
+              <div className={styles['description']}>{project.team}</div>
+            </div>
+            <div className={styles['title-container']}>
+              <div className={styles['label']}>Category</div>
+              <div className={styles['description']}>{project.category}</div>
             </div>
           </div>
         </div>
-      </div>
-      <div className={styles['project-images-container']}>
-        <ProjectDetailImage />
-        <ProjectDetailImage />
+        <div className={styles['project-images-container']}>
+          {project.images.project.map((detail) => (
+            <ProjectDetailImage
+              mainImages={detail.main_images}
+              additionalImages={detail.additional_images}
+            />
+          ))}
+        </div>
       </div>
     </div>
   )
--- components/projects/ProjectDetailImage.js
+++ components/projects/ProjectDetailImage.js
@@ -1,34 +1,25 @@
 import Image from 'next/image'
 import styles from './ProjectDetailImage.module.css'
-const ProjectDetailImage = () => {
+const ProjectDetailImage = ({ mainImages, additionalImages = [] }) => {
   return (
     <div className={styles['container']}>
       <div className={styles['image-container']}>
-        <Image
-          className={styles['image']}
-          width={440}
-          height={434}
-          src={'/images/projects/project_detail_1.png'}
-        />
-        <Image
-          className={styles['image']}
-          width={440}
-          height={434}
-          src={'/images/projects/project_detail_1_1.png'}
-        />
-        <Image
-          className={styles['image']}
-          width={440}
-          height={434}
-          src={'/images/projects/project_detail_1_2.png'}
-        />
+        {additionalImages.map((image, index) => (
+          <Image
+            key={'detaikImage' + index}
+            className={styles['image']}
+            width={440}
+            height={434}
+            src={image}
+          />
+        ))}
       </div>
       <div className={styles['image-container']}>
         <Image
           className={styles['image-full']}
           width={1338}
           height={878}
-          src={'/images/projects/project_detail_2.png'}
+          src={mainImages}
         />
       </div>
     </div>
